b8492c006be4984e62fc166bd41edd7bc5047796
Diarassouba Mamadou <couda.dm@gmail.com>
2023-05-27 13:03:27 +0000

    demo

---
+++ js/convertissuer.js
@@ -0,0 +1,241 @@
+let montant = document.querySelector('#montant')
+
+let selectbox = document.querySelectorAll('.selectbox select')
+
+
+let to = selectbox[1]
+
+console.log(from, to.sel)
+
+// let montant = document.querySelector('#montant')
+let ValeurFinal = document.querySelector('#finalvalue')
+console.log(selectbox)
+let btn = document.querySelector('.btn')
+
+let ApiKey= '4455e1271a-ad405802dc-rvbh6k'
+
+let url = `https://api.fastforex.io/fetch-all?api_key=${ApiKey}&from=USD`
+
+
+
+const apiKey = '3fa7e81a6ef4d2fcc1e7d718a37956eeb6442f299d980ce883b0025f51003d37'
+let country_list = {
+  "AED" : "AE",
+  "AFN" : "AF",
+  "XCD" : "AG",
+  "ALL" : "AL",
+  "AMD" : "AM",
+  "ANG" : "AN",
+  "AOA" : "AO",
+  "AQD" : "AQ",
+  "ARS" : "AR",
+  "AUD" : "AU",
+  "AZN" : "AZ",
+  "BAM" : "BA",
+  "BBD" : "BB",
+  "BDT" : "BD",
+  "XOF" : "BE",
+  "BGN" : "BG",
+  "BHD" : "BH",
+  "BIF" : "BI",
+  "BMD" : "BM",
+  "BND" : "BN",
+  "BOB" : "BO",
+  "BRL" : "BR",
+  "BSD" : "BS",
+  "NOK" : "BV",
+  "BWP" : "BW",
+  "BYR" : "BY",
+  "BZD" : "BZ",
+  "CAD" : "CA",
+  "CDF" : "CD",
+  "XAF" : "CF",
+  "CHF" : "CH",
+  "CLP" : "CL",
+  "CNY" : "CN",
+  "COP" : "CO",
+  "CRC" : "CR",
+  "CUP" : "CU",
+  "CVE" : "CV",
+  "CYP" : "CY",
+  "CZK" : "CZ",
+  "DJF" : "DJ",
+  "DKK" : "DK",
+  "DOP" : "DO",
+  "DZD" : "DZ",
+  "ECS" : "EC",
+  "EEK" : "EE",
+  "EGP" : "EG",
+  "ETB" : "ET",
+  "EUR" : "FR",
+  "FJD" : "FJ",
+  "FKP" : "FK",
+  "GBP" : "GB",
+  "GEL" : "GE",
+  "GGP" : "GG",
+  "GHS" : "GH",
+  "GIP" : "GI",
+  "GMD" : "GM",
+  "GNF" : "GN",
+  "GTQ" : "GT",
+  "GYD" : "GY",
+  "HKD" : "HK",
+  "HNL" : "HN",
+  "HRK" : "HR",
+  "HTG" : "HT",
+  "HUF" : "HU",
+  "IDR" : "ID",
+  "ILS" : "IL",
+  "INR" : "IN",
+  "IQD" : "IQ",
+  "IRR" : "IR",
+  "ISK" : "IS",
+  "JMD" : "JM",
+  "JOD" : "JO",
+  "JPY" : "JP",
+  "KES" : "KE",
+  "KGS" : "KG",
+  "KHR" : "KH",
+  "KMF" : "KM",
+  "KPW" : "KP",
+  "KRW" : "KR",
+  "KWD" : "KW",
+  "KYD" : "KY",
+  "KZT" : "KZ",
+  "LAK" : "LA",
+  "LBP" : "LB",
+  "LKR" : "LK",
+  "LRD" : "LR",
+  "LSL" : "LS",
+  "LTL" : "LT",
+  "LVL" : "LV",
+  "LYD" : "LY",
+  "MAD" : "MA",
+  "MDL" : "MD",
+  "MGA" : "MG",
+  "MKD" : "MK",
+  "MMK" : "MM",
+  "MNT" : "MN",
+  "MOP" : "MO",
+  "MRO" : "MR",
+  "MTL" : "MT",
+  "MUR" : "MU",
+  "MVR" : "MV",
+  "MWK" : "MW",
+  "MXN" : "MX",
+  "MYR" : "MY",
+  "MZN" : "MZ",
+  "NAD" : "NA",
+  "XPF" : "NC",
+  "NGN" : "NG",
+  "NIO" : "NI",
+  "NPR" : "NP",
+  "NZD" : "NZ",
+  "OMR" : "OM",
+  "PAB" : "PA",
+  "PEN" : "PE",
+  "PGK" : "PG",
+  "PHP" : "PH",
+  "PKR" : "PK",
+  "PLN" : "PL",
+  "PYG" : "PY",
+  "QAR" : "QA",
+  "RON" : "RO",
+  "RSD" : "RS",
+  "RUB" : "RU",
+  "RWF" : "RW",
+  "SAR" : "SA",
+  "SBD" : "SB",
+  "SCR" : "SC",
+  "SDG" : "SD",
+  "SEK" : "SE",
+  "SGD" : "SG",
+  "SKK" : "SK",
+  "SLL" : "SL",
+  "SOS" : "SO",
+  "SRD" : "SR",
+  "STD" : "ST",
+  "SVC" : "SV",
+  "SYP" : "SY",
+  "SZL" : "SZ",
+  "THB" : "TH",
+  "TJS" : "TJ",
+  "TMT" : "TM",
+  "TND" : "TN",
+  "TOP" : "TO",
+  "TRY" : "TR",
+  "TTD" : "TT",
+  "TWD" : "TW",
+  "TZS" : "TZ",
+  "UAH" : "UA",
+  "UGX" : "UG",
+  "USD" : "US",
+  "UYU" : "UY",
+  "UZS" : "UZ",
+  "VEF" : "VE",
+  "VND" : "VN",
+  "VUV" : "VU",
+  "YER" : "YE",
+  "ZAR" : "ZA",
+  "ZMK" : "ZM",
+  "ZWD" : "ZW"
+}
+
+
+
+
+
+for (let i = 0; i < selectbox.length; i++) {
+  for (const count in country_list) {
+    Option = document.createElement('option')
+
+    Option.value = count
+    Option.text = count
+    selectbox[i].add(Option)
+  }
+  
+}
+
+
+
+// async function apiCompare(){
+//     let send = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR?api_key=${apiKey}`);
+    
+// }
+
+
+// fetch(`https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR?api_key=${apiKey}`)
+// .then((rest)=> rest.json()).then((response)=>{
+//   console.log(response)
+
+//   btn.addEventListener('click', (e)=>{
+
+//     e.preventDefault()
+  
+//     ValeurFinal.innerHTML = `valeur en USD : ${parseFloat(montant.value * response.USD)} ; Valeur en JPY :${parseFloat(montant.value * response.JPY)}`
+  
+//   })
+
+
+// })
+
+fetch(`${url}`)
+.then((rest)=> rest.json()).then((response)=>{
+  console.log(response)
+
+  // btn.addEventListener('click', (e)=>{
+
+  //   e.preventDefault()
+  
+  //   ValeurFinal.innerHTML = `valeur en USD : ${parseFloat(montant.value * response.USD)} ; Valeur en JPY :${parseFloat(montant.value * response.JPY)}`
+  
+  // })
+
+
+})
+
+let valinput = null
+let from = selectbox[0].addEventListener('change', ()=>{
+   valinput = selectbox[0].value
+   console.log(valinput)
+})
